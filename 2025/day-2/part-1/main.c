c89f831b5e92b945b0e5f6032bddd4267143e9fd
Thomas Morton <thomasandrewmorton@gmail.com>
2025-12-09 02:10:55 +0000

    Correct solution to day-2/part-1
    Will refactor and tidy this up later...

--- 2025/day-2/part-1/main.c
+++ 2025/day-2/part-1/main.c
@@ -2,8 +2,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
-int count_digits(int64_t n) {
+int count_digits(uint64_t n) {
   if (n == 0) return 1;
 
   int count = 0;
@@ -16,15 +17,52 @@ int count_digits(int64_t n) {
   return count;
 }
 
-int64_t same_length_bounds(int64_t lower, int64_t upper, int n_digits) {
-  int64_t sum = 0;
+int is_same_char_string(char *s) {
+  char first = s[0];
 
-  // Which integers split n_digits into groups of equal size?
+  int x;
+  for (x = 1; x < strlen(s); ++x)
+    if (s[x] != first) return 0;
+
+  return 1;
+}
+
+int special_check(char *s) {
+  char a = s[0];
+  char b = s[1];
+
+  int even = 1;
+
+  int x;
+  for (x = 0; x < strlen(s); ++x) {
+    if (even)
+      if (s[x] != a) return 0; else even = 0;
+    else
+      if (s[x] != b) return 0; else even = 1; 
+  }
+
+  return 1;
+}
+
+uint64_t same_length_bounds(uint64_t lower, uint64_t upper) {
+  uint64_t sum = 0;
+
+  int n_digits = count_digits(upper);
+
+  if (n_digits % 2 != 0) {
+    printf("  Odd digits in interval. Skipping!\n\n");
+    return sum;
+  }
+
   int n_groups;
   for (n_groups = n_digits; n_groups >= 2; --n_groups) {
+    // Which integers split n_digits into groups of equal size?
     if (n_digits % n_groups != 0) continue;
     
     int size = n_digits / n_groups;
+
+    // Skip odd number of groups.
+    if (n_groups % 2 != 0) continue;
 
     // Get string representations of interval bounds.
     char lower_string[n_digits + 1];
@@ -34,48 +72,138 @@ int64_t same_length_bounds(int64_t lower, int64_t upper, int n_digits) {
     snprintf(upper_string, sizeof(upper_string), "%ld", upper);
 
     // The first group is the repeating pattern (first 'size' digits). 
-    char    pattern_string[size + 1];
-    int64_t pattern_value;
+    char     pattern_string[size + 1];
+    uint64_t pattern_value;
 
     strncpy(pattern_string, lower_string, size);
     pattern_string[size] = '\0';
     pattern_value = strtoll(pattern_string, NULL, 10);
 
-    printf("%d -> %d groups of size %d (%ld)\n", n_digits, n_groups, size, pattern_value);
+    printf("  %d groups of size %d\n", n_groups, size);
 
-    // Generate a candidate ID (pattern repeated n_groups times).
-    char    candidate_string[n_digits + 1];
-    int64_t candidate_value;
+    int first = 1;
 
-    int x;
-    strcpy(candidate_string, pattern_string);
-    for (x = 0; x < n_groups - 1; ++x)
-      strcat(candidate_string, pattern_string);
-    candidate_string[n_digits] = '\0';
-    candidate_value = strtoll(candidate_string, NULL, 10);
+    do {
+      if (!first)
+        snprintf(pattern_string, sizeof(pattern_string), "%ld", pattern_value);
+      else
+        first = 0;
 
-    // If the candidate ID is within the interval, it is invalid!
-    if (candidate_value >= lower && candidate_value <= upper) {
-      printf("Accepted: %ld\n", candidate_value);
-      sum += candidate_value;
-    }
-    else
-      printf("Rejected: %ld\n", candidate_value);
+      // Generate a candidate ID (the pattern repeated n_groups times).
+      char     candidate_string[n_digits + 1];
+      uint64_t candidate_value;
+
+      int x;
+      strcpy(candidate_string, pattern_string);
+      for (x = 0; x < n_groups - 1; ++x)
+        strcat(candidate_string, pattern_string);
+      candidate_string[n_digits] = '\0';
+      candidate_value = strtoll(candidate_string, NULL, 10);
+
+      // The candidate violates the upper bound. This pattern size is exhausted.
+      if (candidate_value > upper) {
+        printf("    Rejected: %ld\n\n", candidate_value);
+        break;
+      }
+      // The candidate violates the lower bound.
+      else if (candidate_value < lower) {
+        printf("    Rejected: %ld\n", candidate_value);
+        
+        if   (count_digits(pattern_value + 1) > count_digits(pattern_value)) break;
+        else pattern_value++;
+      }
+      // The candidate is within the interval, so it is invalid!
+      else {
+        // First check that the candidate has not already been accepted.
+        if (size != 1 && is_same_char_string(candidate_string)) {
+          printf("    Rejected: %ld (duplicate)\n", candidate_value);
+          
+          if   (count_digits(pattern_value + 1) > count_digits(pattern_value)) break;
+          else pattern_value++;
+
+          continue;
+        }
+
+        if (n_groups == 2 && size > 2 && special_check(candidate_string)) {
+           printf("    Rejected: %ld (duplicate)\n", candidate_value);
+          
+          if   (count_digits(pattern_value + 1) > count_digits(pattern_value)) break;
+          else pattern_value++;
+
+          continue; 
+        }
+
+        printf("    Accepted: %ld\n", candidate_value);
+        sum += candidate_value;
+
+        if   (count_digits(pattern_value + 1) > count_digits(pattern_value)) break;
+        else pattern_value++;
+      }
+    } while (1);
   }
 
   return sum;
 }
 
-int64_t sum_invalid_ids(int64_t lower, int64_t upper) {
+uint64_t different_length_bounds(uint64_t lower, uint64_t upper) {
+  uint64_t sum = 0;
+
+  /* 
+   * Break down the problem to reuse same_length_bounds()
+   *
+   * The interval 0-9999 can be decomposed into 4 non-overlapping intervals:
+   *
+   * 1) 0-9
+   * 2) 10-99
+   * 3) 100-999
+   * 4) 1000-9999
+   */
+
   // Get number of digits in each bound.
   int n_digits_lower = count_digits(lower);
   int n_digits_upper = count_digits(upper);
 
-  if (n_digits_lower == n_digits_upper)
-    return same_length_bounds(lower, upper, n_digits_upper);
+  uint64_t l = 0;
+  uint64_t u = 9;
 
+  int x;
+  for (x = 1; x <= n_digits_upper; ++x) {
+    int n_digits_l = count_digits(l);
+
+    if (n_digits_l < n_digits_lower) {
+      ;
+    }
+    else if (n_digits_l == n_digits_lower) {
+      printf("Sub-interval: %ld-%ld\n", lower, u);
+      sum += same_length_bounds(lower, u);
+    }
+    else if (n_digits_l == n_digits_upper) {
+      printf("Sub-interval: %ld-%ld\n", l, upper);
+      sum += same_length_bounds(l, upper);
+    }
+    else {
+      printf("Sub-interval: %ld-%ld\n", l, u);
+      sum += same_length_bounds(l, u);
+    }
+
+    l = pow(10, x);
+    u = pow(10, (x + 1)) - 1;
+  }
+
+  return sum;
+}
+
+uint64_t sum_invalid_ids(uint64_t lower, uint64_t upper) {
+  // Get number of digits in each bound.
+  int n_digits_lower = count_digits(lower);
+  int n_digits_upper = count_digits(upper);
+
+  printf("Interval: %ld-%ld\n", lower, upper);
+
+  if (n_digits_lower == n_digits_upper)
+    return same_length_bounds(lower, upper);
   else
-    return 0;
+    return different_length_bounds(lower, upper);
 }
 
 int main(int argc, char **argv) {
@@ -94,13 +222,13 @@ int main(int argc, char **argv) {
   }
 
   // Lower bound.
-  int64_t lower;
+  uint64_t lower;
   
   // Upper bound.
-  int64_t upper;
+  uint64_t upper;
 
   // Sum of all invalid IDs.
-  int64_t sum = 0;
+  uint64_t sum = 0;
 
   // Read one interval from the file at a time and process it.
   while (!feof(input)) {
@@ -121,13 +249,10 @@ int main(int argc, char **argv) {
 
     // Consume the comma delimiter or new line character.
     char c = fgetc(input);
-    if (c == '\n') break;
-
-    printf("Interval: %ld-%ld\n", lower, upper);
 
     sum += sum_invalid_ids(lower, upper);
 
-    break;
+    if (c == '\n') break;
   }
 
   fclose(input);
